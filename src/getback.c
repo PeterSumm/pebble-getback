d80e906613d35ee5b904103993412a45243ff699
PeterSumm <peter@summers.aus.net>
2015-03-21 09:03:39 +1100

    Update getback.c

--- src/getback.c
+++ src/getback.c
@@ -1,7 +1,9 @@
 #include <pebble.h>
+#undef APP_LOG
+#define APP_LOG(...)
 
 static Window *window;
-static TextLayer *dist_layer;
+static TextLayer *distance_layer;
 static TextLayer *unit_layer;
 static TextLayer *hint_layer;
 static Layer *head_layer;
@@ -9,20 +11,28 @@ static TextLayer *n_layer;
 static TextLayer *e_layer;
 static TextLayer *s_layer;
 static TextLayer *w_layer;
+static TextLayer *heading_layer;
 static TextLayer *direction_layer;
 static TextLayer *bearing_layer;
+static TextLayer *accuracy_layer;
+static TextLayer *speed_layer;
 int32_t distance = 0;
+int16_t bearing = 0;
 int16_t heading = 0;
 int16_t orientation = 0;
+int16_t interval = 5;
 static const uint32_t CMD_KEY = 0x1;
-static const uint32_t HEAD_KEY = 0x2;
-static const uint32_t DIST_KEY = 0x3;
-static const uint32_t UNITS_KEY = 0x4;
-static char command[] = "kick";
+static const uint32_t BEARING_KEY = 0x2;
+static const uint32_t DISTANCE_KEY = 0x3;
+static const uint32_t UNIT_KEY = 0x4;
+static const uint32_t INTERVAL_KEY = 0x5;
+static const uint32_t SPEED_KEY = 0x6;
+static const uint32_t ACCURACY_KEY = 0x7;
+static const uint32_t HEADING_KEY = 0x8;
+static char command[6] = "get";
 static GPath *head_path;
 static GRect hint_layer_size;
-static const double YARD_LENGTH = 0.9144;
-static const int32_t YARDS_IN_MILE = 1760;
+static AppTimer *locationtimer;
 bool updated = true;
 GPoint center;
 
@@ -35,17 +45,41 @@ const GPathInfo HEAD_PATH_POINTS = {
   }
 };
 
+static void tick_handler(void *data) {
+  if (interval == 0) return;
+  text_layer_set_text(speed_layer, "?");
+  strcpy(command,"get");
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  if (iter == NULL) {
+    vibes_short_pulse();
+    APP_LOG(APP_LOG_LEVEL_WARNING, "Can not send command '%s' to phone!", command);
+  } else {
+    dict_write_cstring(iter, CMD_KEY, command);
+    const uint32_t final_size = dict_write_end(iter);
+    app_message_outbox_send();
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Sent command '%s' to phone! (%d bytes)", command, (int) final_size);
+  }
+  static char interval_buf[8];
+  snprintf(interval_buf, sizeof(interval_buf), "%ds", interval);
+  text_layer_set_text(accuracy_layer, interval_buf);
+  
+  locationtimer = app_timer_register(1000 * interval, (AppTimerCallback) tick_handler, NULL);
+}
+  
 static void reset_handler(ClickRecognizerRef recognizer, void *context) {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Reset");
   if (hint_layer) {
     text_layer_destroy(hint_layer);
     hint_layer = NULL;
   }
+  app_timer_reschedule(locationtimer, 1000 * (interval + 7));
   strcpy(command, "set");
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
   if (iter == NULL) {
-    APP_LOG(APP_LOG_LEVEL_WARNING, "Can not send %s to phone!", command);
+    vibes_short_pulse();
+    APP_LOG(APP_LOG_LEVEL_WARNING, "Can not send command %s to phone!", command);
     return;
   }
   dict_write_cstring(iter, CMD_KEY, command);
@@ -59,12 +93,13 @@ static void hint_handler(ClickRecognizerRef recognizer, void *context) {
     text_layer_destroy(hint_layer);
     hint_layer = NULL;
   } else {
-    Layer *window_layer = window_get_root_layer(window);
-    hint_layer = text_layer_create(hint_layer_size);
-    text_layer_set_font(hint_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-    text_layer_set_text_alignment(hint_layer, GTextAlignmentCenter);
-    layer_add_child(window_layer, text_layer_get_layer(hint_layer));
-    text_layer_set_text(hint_layer, "Press and hold to set target to current position.");
+    tick_handler(NULL);
+//    Layer *window_layer = window_get_root_layer(window);
+//    hint_layer = text_layer_create(hint_layer_size);
+//    text_layer_set_font(hint_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+//    text_layer_set_text_alignment(hint_layer, GTextAlignmentCenter);
+//    layer_add_child(window_layer, text_layer_get_layer(hint_layer));
+//    text_layer_set_text(hint_layer, "Press and hold to set target to current position.");
   }  
 }  
 
@@ -78,7 +113,7 @@ void out_sent_handler(DictionaryIterator *sent, void *context) {
     text_layer_set_text_alignment(hint_layer, GTextAlignmentCenter);
     layer_add_child(window_layer, text_layer_get_layer(hint_layer));
     text_layer_set_text(hint_layer, "Target set. Press again to continue.");
-    vibes_short_pulse();
+    vibes_long_pulse();
   };
 }
 
@@ -95,85 +130,87 @@ void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, voi
       text_layer_set_text(hint_layer, "Cannot set target. Please restart the app.");
     };
   }
-  vibes_short_pulse();
+//  vibes_double_pulse();
 }
 
 void in_received_handler(DictionaryIterator *iter, void *context) {
   // incoming message received
-  static char units[9];
-  static char dist_text[9];
+  static char unit_text[9];
+  static char distance_text[9];
   static char bearing_text[9];
+  static char heading_text[9];
+  static char speed_text[9];
+  static char accuracy_text[12];
+  
   updated = true;
-  Tuple *head_tuple = dict_find(iter, HEAD_KEY);
-  if (head_tuple) {
-    heading = head_tuple->value->int16;
-    text_layer_set_text(direction_layer, ".");
-    snprintf(bearing_text, sizeof(bearing_text), "%d", (int) (360 + heading) % 360);
+  
+  Tuple *bearing_tuple = dict_find(iter, BEARING_KEY);
+  if (bearing_tuple) {
+    bearing = bearing_tuple->value->int16;
+    snprintf(bearing_text, sizeof(bearing_text), "%d", bearing);
     text_layer_set_text(bearing_layer, bearing_text);
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Updated heading to %d", (int) heading);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Updated bearing to %d", bearing);
     layer_mark_dirty(head_layer);
   }
-  Tuple *units_tuple = dict_find(iter, UNITS_KEY);
-  if (units_tuple) {
-    strcpy(units, units_tuple->value->cstring);
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Units: %s", units);
+
+  Tuple *unit_tuple = dict_find(iter, UNIT_KEY);
+  if (unit_tuple) {
+    strcpy(unit_text, unit_tuple->value->cstring);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Unit: %s", unit_text);
   }
-  Tuple *dist_tuple = dict_find(iter, DIST_KEY);
-  if (dist_tuple) {
-    distance = dist_tuple->value->int32;
-    if (strcmp(units, "imperial") == 0) {
-      distance = distance / YARD_LENGTH;
-      if (distance >= 100 * YARDS_IN_MILE) {
-        snprintf(dist_text, sizeof(dist_text), "%d", (int) (distance/YARDS_IN_MILE));
-        text_layer_set_text(unit_layer, "mi");
-      } else { 
-        if (distance >= 10 * YARDS_IN_MILE) {
-          snprintf(dist_text, sizeof(dist_text), "%d.%d", (int) (distance/YARDS_IN_MILE), (int) (10 * (distance % YARDS_IN_MILE) / YARDS_IN_MILE));
-          text_layer_set_text(unit_layer, "mi");
-        } else {
-          if (distance >= YARDS_IN_MILE / 2) {
-            snprintf(dist_text, sizeof(dist_text), "%d.%d%d", (int) (distance/YARDS_IN_MILE), (int) (10 * (distance % YARDS_IN_MILE) / YARDS_IN_MILE), 
-              (int) (100 * (distance % (YARDS_IN_MILE / 10))/ YARDS_IN_MILE));
-            text_layer_set_text(unit_layer, "mi");
-          } else {
-            snprintf(dist_text, sizeof(dist_text), "%d", (int) (distance));
-            text_layer_set_text(unit_layer, "yd");
-          }
-        } 
-      }
-    } else {
-      if (distance >= 100000) {
-        snprintf(dist_text, sizeof(dist_text), "%d", (int) (distance/1000));
-        text_layer_set_text(unit_layer, "km");
-      } else { 
-        if (distance >= 10000) {
-          snprintf(dist_text, sizeof(dist_text), "%d.%d", (int) (distance/1000), (int) ((distance % 1000)/100));
-          text_layer_set_text(unit_layer, "km");
-        } else {
-          if (distance >= 999) {
-            snprintf(dist_text, sizeof(dist_text), "%d.%d%d", (int) (distance/1000), (int) ((distance % 1000)/100), (int) ((distance % 100)/10));
-            text_layer_set_text(unit_layer, "km");
-          } else {
-            snprintf(dist_text, sizeof(dist_text), "%d", (int) (distance));
-            text_layer_set_text(unit_layer, "m");
-          }
-        } 
-      }
-    }
+  text_layer_set_text(unit_layer, unit_text);
+
+  Tuple *distance_tuple = dict_find(iter, DISTANCE_KEY);
+  if (distance_tuple) {
+    strcpy(distance_text, distance_tuple->value->cstring);
    }
-  text_layer_set_text(dist_layer, dist_text);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Distance updated: %d %s", (int) distance, text_layer_get_text(unit_layer));
+  text_layer_set_text(distance_layer, distance_text);
+  
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Distance updated: %s %s", dist_text, text_layer_get_text(unit_layer));
+  
+  Tuple *interval_tuple = dict_find(iter, INTERVAL_KEY);
+  if (interval_tuple) {
+    interval = interval_tuple->value->int32;
+  }
+  APP_LOG(APP_LOG_LEVEL_DEBUG,"New interval is %d.", interval);
+ 
+  Tuple *speed_tuple = dict_find(iter, SPEED_KEY);
+  if (speed_tuple) {
+    strcpy(speed_text, speed_tuple->value->cstring);
+    text_layer_set_text(speed_layer, speed_text);
+    APP_LOG(APP_LOG_LEVEL_DEBUG,"Speed is %s.", speed_text);
+  }
+  
+  Tuple *accuracy_tuple = dict_find(iter, ACCURACY_KEY);
+  if (accuracy_tuple) {
+    strcpy(accuracy_text, accuracy_tuple->value->cstring);
+    text_layer_set_text(accuracy_layer, accuracy_text);
+    APP_LOG(APP_LOG_LEVEL_DEBUG,"Accuracy is %s.", accuracy_text);
+  }
+  
+  Tuple *heading_tuple = dict_find(iter, HEADING_KEY);
+  if (heading_tuple) {
+    heading = heading_tuple->value->int16;
+    if (heading >= 0) {
+      snprintf(heading_text, sizeof(heading_text), "%d", heading);
+      text_layer_set_text(heading_layer, heading_text);
+    } else
+      text_layer_set_text(heading_layer, "");
+      
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Updated heading to %d", heading);
+  }
 }
 
-void compass_heading_handler(CompassHeadingData heading_data){
-  static char direction_buf[5];
-  orientation = heading_data.true_heading;
-  switch (heading_data.compass_status) {
+void compass_direction_handler(CompassHeadingData direction_data){
+  static char direction_buf[]="~~~";
+  orientation = direction_data.true_heading;
+  switch (direction_data.compass_status) {
     case CompassStatusDataInvalid:
-      snprintf(direction_buf, sizeof(direction_buf), "%s", "!");
+      snprintf(direction_buf, sizeof(direction_buf), "%s", "!!!");
       return;
     case CompassStatusCalibrating:
-      snprintf(direction_buf, sizeof(direction_buf), "%s", "?");
+      snprintf(direction_buf, sizeof(direction_buf), "%s", "???");
       break;
     case CompassStatusCalibrated:
       snprintf(direction_buf, sizeof(direction_buf), "%d", (int) ((360-TRIGANGLE_TO_DEG(orientation)) % 360));
@@ -195,7 +232,7 @@ void compass_heading_handler(CompassHeadingData heading_data){
 }
 
 static void head_layer_update_callback(Layer *layer, GContext *ctx) {
-  gpath_rotate_to(head_path, (TRIG_MAX_ANGLE / 360) * (heading + TRIGANGLE_TO_DEG(orientation)));
+  gpath_rotate_to(head_path, (TRIG_MAX_ANGLE / 360) * (bearing + TRIGANGLE_TO_DEG(orientation)));
   graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_fill_circle(ctx, center, 77);
   graphics_context_set_fill_color(ctx, GColorWhite);
@@ -261,41 +298,64 @@ static void window_load(Window *window) {
   text_layer_set_text(w_layer, "W");
   layer_add_child(head_layer, text_layer_get_layer(w_layer));
 
-  dist_layer = text_layer_create(GRect(0, 48, 144, 42));
-  text_layer_set_background_color(dist_layer, GColorClear);
-  text_layer_set_font(dist_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(dist_layer, GTextAlignmentCenter);
-  text_layer_set_text(dist_layer, "");
-  layer_add_child(window_layer, text_layer_get_layer(dist_layer));
+  distance_layer = text_layer_create(GRect(0, 48, 144, 42));
+  text_layer_set_background_color(distance_layer, GColorClear);
+  text_layer_set_font(distance_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(distance_layer, GTextAlignmentCenter);
+  text_layer_set_text(distance_layer, "...");
+  layer_add_child(window_layer, text_layer_get_layer(distance_layer));
   
   unit_layer = text_layer_create(GRect(50, 84, 44, 30));
   text_layer_set_background_color(unit_layer, GColorClear);
   text_layer_set_font(unit_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment(unit_layer, GTextAlignmentCenter);
-  text_layer_set_text(unit_layer, "");
+  text_layer_set_text(unit_layer, "...");
   layer_add_child(window_layer, text_layer_get_layer(unit_layer));
 
   direction_layer = text_layer_create(GRect(90, 132, 53, 18));
   text_layer_set_background_color(direction_layer, GColorClear);
   text_layer_set_font(direction_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   text_layer_set_text_alignment(direction_layer, GTextAlignmentRight);
-  text_layer_set_text(direction_layer, ".");
+  text_layer_set_text(direction_layer, "...");
   layer_add_child(window_layer, text_layer_get_layer(direction_layer));
 
   bearing_layer = text_layer_create(GRect(0, 132, 53, 18));
   text_layer_set_background_color(bearing_layer, GColorClear);
   text_layer_set_font(bearing_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   text_layer_set_text_alignment(bearing_layer, GTextAlignmentLeft);
-  text_layer_set_text(direction_layer, "!!!");
+  text_layer_set_text(direction_layer, "...");
   layer_add_child(window_layer, text_layer_get_layer(bearing_layer));
+
+  speed_layer = text_layer_create(GRect(40, 26, 64, 30));
+  text_layer_set_background_color(speed_layer, GColorClear);
+  text_layer_set_font(speed_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_alignment(speed_layer, GTextAlignmentCenter);
+  text_layer_set_text(speed_layer, "...");
+  layer_add_child(window_layer, text_layer_get_layer(speed_layer));
+
+  accuracy_layer = text_layer_create(GRect(90, -4, 53, 18));
+  text_layer_set_background_color(accuracy_layer, GColorClear);
+  text_layer_set_font(accuracy_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_alignment(accuracy_layer, GTextAlignmentRight);
+  text_layer_set_text(accuracy_layer, "...");
+  layer_add_child(window_layer, text_layer_get_layer(accuracy_layer));
+
+  heading_layer = text_layer_create(GRect(0, -4, 53, 18));
+  text_layer_set_background_color(heading_layer, GColorClear);
+  text_layer_set_font(heading_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_alignment(heading_layer, GTextAlignmentLeft);
+  text_layer_set_text(heading_layer, "...");
+  layer_add_child(window_layer, text_layer_get_layer(heading_layer));
 }
 
 static void window_unload(Window *window) {
-  layer_destroy(head_layer);
+  text_layer_destroy(heading_layer);
+  text_layer_destroy(speed_layer);
+  text_layer_destroy(accuracy_layer);
   text_layer_destroy(bearing_layer);
   text_layer_destroy(direction_layer);
   text_layer_destroy(unit_layer);
-  text_layer_destroy(dist_layer);
+  text_layer_destroy(distance_layer);
   text_layer_destroy(w_layer);
   text_layer_destroy(s_layer);
   text_layer_destroy(e_layer);
@@ -305,29 +365,10 @@ static void window_unload(Window *window) {
   }
 }
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  
-  if (!updated){
-    // No update in the last minute, better give the Javascript a kick in the guts.
-    DictionaryIterator *iter;
-    app_message_outbox_begin(&iter);
-    if (iter == NULL) {
-      APP_LOG(APP_LOG_LEVEL_WARNING, "Can not send kick command to phone!");
-      return;
-    }
-    strcpy(command,"kick");
-    dict_write_cstring(iter, CMD_KEY, command);
-    const uint32_t final_size = dict_write_end(iter);
-    app_message_outbox_send();
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Sent command '%s' to phone! (%d bytes)", command, (int) final_size);
-  }
-  updated = false;
-}
-  
 static void init(void) {
   // update screen only when heading changes at least 3 degrees
   compass_service_set_heading_filter(TRIG_MAX_ANGLE*3/360);
-  compass_service_subscribe(&compass_heading_handler);
+  compass_service_subscribe(&compass_direction_handler);
   app_message_register_inbox_received(in_received_handler);
   app_message_register_inbox_dropped(in_dropped_handler);
   app_message_register_outbox_sent(out_sent_handler);
@@ -346,10 +387,14 @@ static void init(void) {
   window_stack_push(window, animated);
 
   // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  // tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  // locationtimer = app_timer_register	(	1000 * interval, (AppTimerCallback) tick_handler, NULL);
+  
+    locationtimer = app_timer_register	(	5000, (AppTimerCallback) tick_handler, NULL);
 }
 
 static void deinit(void) {
+  app_timer_cancel(locationtimer);
   compass_service_unsubscribe();
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
@@ -363,7 +408,6 @@ static void deinit(void) {
   const uint32_t final_size = dict_write_end(iter);
   app_message_outbox_send();
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Sent command '%s' to phone! (%d bytes)", command, (int) final_size);
-
   window_destroy(window);
 }
 
